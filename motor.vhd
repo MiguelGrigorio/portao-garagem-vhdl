a5668e530cd4d97666cd43af378b45316cfad134
Miguel Grigorio <142750268+MiguelGrigorio@users.noreply.github.com>
2023-10-17 16:47:28 -0300

    Update motor.vhd

--- motor.vhd
+++ motor.vhd
@@ -10,7 +10,7 @@ entity motor is
 		enable    : in std_logic;                     -- 	Pode realizar o movimento ou não
 		angulo    : in integer;                       --	Angulo que vai girar
 		wires     : out std_logic_vector(3 downto 0); -- 	Bobinas motor
-		overflow  : out std_logic
+		step 	  : out integer
 	);
 end motor;
 
@@ -56,22 +56,17 @@ begin
 		if reset = '1' then
 			state    <= Blue;
 			steps    <= 0;
-			overflow <= '0';
 		elsif rising_edge(clock) then
+			step <= steps;
 			if ovf = '1' and enable = '1' then
 				state    <= next_state;
-				overflow <= '0';
 				if direction = '0' then
 					if steps < angulo then
 						steps <= steps + 1;
-					else
-						overflow <= '1';
 					end if;
 				else
 					if steps > 0 then
 						steps <= steps - 1;
-					else
-						overflow <= '1';
 					end if;
 				end if;
 			end if;
